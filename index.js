123d1e433090eb8df8f0d31440a3d7e3846d5e19
jsun <jaysun054@gmail.com>
2021-09-27 13:52:35 -0700

    test

--- index.js
+++ index.js
@@ -53,11 +53,22 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('readme created!');
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions);
+    inquirer.prompt(questions)
+    .then(answers => {
+        console.log(answers);
+        writeToFile("README.md", generate(answers));
+    });
 }
 
 // Function call to initialize app
