af8020d025fa05885351d68c04ac52a38d136510
jsun <jaysun054@gmail.com>
2021-09-27 15:10:37 -0700

    added md details

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,31 +13,63 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+  ${data.description}
   <!-- TABLE OF CONTENTS -->
   <details open="open">
-    <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+    <summary><h3 style="display: inline-block">Table of Contents</h3></summary>
     <ol>
-      <li>
-        <a href="#about-the-project">About The Project</a>
-        <ul>
-          <li><a href="#built-with">Built With</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="#getting-started">Getting Started</a>
-        <ul>
-          <li><a href="#prerequisites">Prerequisites</a></li>
-          <li><a href="#installation">Installation</a></li>
-        </ul>
-      </li>
+      <li><a href="#description">Description</a></li>
+      <li><a href="#installation">Installation</a></li>
       <li><a href="#usage">Usage</a></li>
-      <li><a href="#roadmap">Roadmap</a></li>
-      <li><a href="#contributing">Contributing</a></li>
+      <li><a href="#contribution">Contribution</a></li>
+      <li><a href="#test">Test</a></li>
       <li><a href="#license">License</a></li>
-      <li><a href="#contact">Contact</a></li>
-      <li><a href="#acknowledgements">Acknowledgements</a></li>
+      <li><a href="#github">Github</a></li>
+      <li><a href="#email">Email</a></li>
     </ol>
   </details>
+
+  ## Installation
+  To get a local copy up and running follow these simple steps.
+
+  This is an example of how to list things you need to use the software and how to install them:
+  <br/>
+
+    ${data.installation}
+  
+  ## Usage
+  _For more examples, please consider:_
+  <br/>
+
+    ${data.usage}
+
+  ## Contribution
+  For Example:
+
+    Fork the Project
+
+    Create your Feature Branch ("git checkout -b feature/AmazingFeature")
+
+    Commit your Changes ("git commit -m 'Add some AmazingFeature'")
+
+    Push to the Branch ("git push origin feature/AmazingFeature")
+
+    Open a Pull Request
+  
+  Additionally: ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## License
+  ${data.license}
+
+  ## Github
+  Project: ${data.github}
+
+  ## Email
+  Email me: ${data.email}
 `;
 }
 
