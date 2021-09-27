18f8d4d8e7107ade211245d76932bf06f044f6fd
jsun <jaysun054@gmail.com>
2021-09-27 12:52:21 -0700

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generate = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter a project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter any installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please enter any contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please enter any test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Please choose a license:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter a Github username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter a email address:'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
